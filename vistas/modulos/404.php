27db61117854d38ba0f08c909a73f7b148eb2c04
Jorge <jlcch789@gmail.com>
2024-03-07 12:44:20 -0400

    prueba

---
+++ vistas/modulos/404.php
@@ -0,0 +1,34 @@
+<!-- Contenido de la pagina -->
+<div class="content-wrapper">
+    <!-- Contenido cabecera -->
+    <section class="content-header">
+        <h1>
+            Página no encontrada
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+            <li class="active">Página no encontrada</li>
+        </ol>
+    </section>
+
+    <!-- contenido principal -->
+    <section class="content">
+        <div class="error-page">
+            <h2 class="headline text-primary">404</h2>
+
+            <div class="error-content">
+                <h3>
+                    <i class="fa fa-warning text-yellow"></i>
+                    ¡Ups! Página no encontrada.
+                </h3>
+
+                <p>
+                    Ingresa al menu lateral y de alli podras encontrar las paginas disponibles.    
+                    
+                    No pudimos encontrar la página que estabas buscando.
+                    También puedes regresar haciendo <a href="inicio">click aqui.</a>
+                </p>
+            </div>
+        </div>
+    </section>
+</div>
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+
+    <strong>Copyright &copy;2024
+        <a href="http://www.google.com">Google</a>
+    </strong>
+    Todos los derechos reservados.
+</footer>
